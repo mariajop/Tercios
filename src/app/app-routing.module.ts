07b78adf551239322f4b43351477fa0bcd079721
María José Plaza <m.plazasoriano@metro-markets.de>
2023-01-21 16:12:31 +0100

    Add minor format changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./components/home/home.component";
-import {NewsComponent} from "./components/news/news.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NewsComponent } from "./components/news/news.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'news', component: NewsComponent}
+  { path: '', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
 ];
 
 @NgModule({
